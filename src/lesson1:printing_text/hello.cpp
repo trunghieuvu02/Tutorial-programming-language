d37a24be77bf4691bcbbf3fd305c10babca0c967
trunghieuvu02 <mrtrunghieu139@gmail.com>
2024-01-04 17:00:40 +0000

    [ADD] src folder

---
+++ src/lesson1:printing_text/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "I love bacon roll \n" << endl;
+	cout << "and tea" << endl;
+
+	return 0;
+}
---
+++ src/lesson1:printing_text/variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 6;
+	int b = 21;
+
+	int sum = a + b;	
+	cout << sum <<endl;
+
+	return 0;	
+}
